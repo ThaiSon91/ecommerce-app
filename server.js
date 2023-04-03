68fb466edf92477efe108c123723a90668a12e6c
ThaiSonDev <sonthaidev91@gmail.com>
2023-04-03 23:03:45 +0700

    settup project

---
+++ server.js
@@ -0,0 +1,28 @@
+// const colors = require("colors");
+// const express = require("express");
+
+//type la module
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+
+//comfig env
+dotenv.config();
+
+//rest object
+const app = express();
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+  );
+});
