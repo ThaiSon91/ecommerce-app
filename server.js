948edb99891ecc02e8fd838af34d2fceacbc413e
ThaiSonDev <sonthaidev91@gmail.com>
2023-04-04 20:45:45 +0700

    connect to MongoDB

--- server.js
+++ server.js
@@ -5,12 +5,21 @@
 import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //comfig env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
 
 //rest api
 app.get("/", (req, res) => {
