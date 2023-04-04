16f672acabb526f643afd29af398e6f1ccb4619c
ThaiSonDev <sonthaidev91@gmail.com>
2023-04-04 21:30:17 +0700

    model and hashing password

---
+++ routers/auth.router.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { registerController } from "../controllers/auth.controller.js";
+
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+
+router.post("./register", registerController);
+
+export default router;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRouters from "./routers/auth.router.js";
 
 //comfig env
 dotenv.config();
@@ -20,6 +21,9 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routers
+app.use("/api/v1/auth", authRouters);
 
 //rest api
 app.get("/", (req, res) => {
