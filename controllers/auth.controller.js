10d6d017f6c7acf06b8756a69a846d6d0e03950c
ThaiSonDev <sonthaidev91@gmail.com>
2023-04-05 22:51:08 +0700

    protected middlewwares

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -113,3 +113,13 @@ export const loginController = async (req, res) => {
     });
   }
 };
+
+//test controller
+export const testController = (req, res) => {
+  try {
+    res.send("Protected Routes");
+  } catch (error) {
+    console.log(error);
+    res.send({ error });
+  }
+};
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
   registerController,
   loginController,
+  testController,
 } from "../controllers/auth.controller.js";
+import { requireSignIn, isAdmin } from "../middlewares/auth.middlewares.js";
 
 //router object
 const router = express.Router();
@@ -13,5 +15,8 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginController);
+
+//test routes
+router.get("/test", requireSignIn, isAdmin, testController);
 
 export default router;
