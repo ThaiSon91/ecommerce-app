479059df661ebf5c81d196b91bdb0743d868d96c
ThaiSonDev <sonthaidev91@gmail.com>
2023-04-14 23:45:07 +0700

    Get Update Delete product p2

--- client/src/pages/Admin/Product.js
+++ client/src/pages/Admin/Product.js
@@ -1,8 +1,28 @@
 import Layout from "../../components/layout/layout";
 import AdminMenu from "../../components/layout/AdminMenu";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
-const Product = () => {
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  //get all products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  //lifecycle method
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <Layout>
       <div className="row">
@@ -11,10 +31,31 @@ const Product = () => {
         </div>
         <div className="col-md-9">
           <h1 className="text-center">All Products List</h1>
+          <div className="d-flex">
+            {products?.map((p) => (
+              <Link
+                key={p._id}
+                to={`/dashboard/admin/product/${p.slug}`}
+                className="product-link"
+              >
+                <div className="card m-2" style={{ width: "18rem" }}>
+                  <img
+                    src={`/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p.name}</h5>
+                    <p className="card-text">{p.description}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
   );
 };
 
-export default Product;
+export default Products;
