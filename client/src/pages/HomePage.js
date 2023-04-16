d645e4727207b50b87d4f7b7fe02c780c589c8b9
ThaiSonDev <sonthaidev91@gmail.com>
2023-04-16 10:02:08 +0700

    Category Price Pagnination Filter p2

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/layout/layout";
-import { useAuth } from "../context/auth";
 import axios from "axios";
+import { Checkbox } from "antd";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
 
   //get products
   const getAllProducts = async () => {
@@ -22,18 +38,56 @@ const HomePage = () => {
     getAllProducts();
   }, []);
 
+  //filter by cat
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
   return (
     <Layout title={"Best offers"}>
       {/* <h1>HomePage</h1>
       <pre>{JSON.stringify(auth, null, 4)}</pre> */}
       <div className="row mt-3">
-        <div className="col-md-3">
+        <div className="col-md-2">
           <h4 className="text-center">Filter By Category</h4>
+          <div className="d-flex flex-column">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
         </div>
         <div className="col-md-9">
+          {JSON.stringify(checked, null, 4)}
           <h1 className="text-center">All Products</h1>
           <div className="d-flex flex-wrap">
-            <h1>products</h1>
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <button className="btn btn-primary ms-1">More Details</button>
+                  <button className="btn btn-secondary ms-1">
+                    ADD TO CARD
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
